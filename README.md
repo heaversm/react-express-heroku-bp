86bbfab2fae17d34b61ed443b90124b5fcd27c4d
Mike Heavers <mheavers@gmail.com>
2022-06-01 10:54:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Express Heroku Boilerplate
+
+This is a basic starter for React (Create React App) and Express, to be deployed on Heroku. You can follow [this gist](https://gist.github.com/heaversm/1db85cf40bfb6e6157d7fe442e24aa4d) to create this from scratch or see how it was built.
---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
