2c1aef29e6aa6f844d8337cf659fda3ffd1bf322
Mike Heavers <mheavers@gmail.com>
2022-06-01 12:55:43 -0700

    add sample api fetch request add documentation for heroku add dotenv and axios to backend cleanup

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # React Express Heroku Boilerplate
 
 This is a basic starter for React (Create React App) and Express, to be deployed on Heroku. You can follow [this gist](https://gist.github.com/heaversm/1db85cf40bfb6e6157d7fe442e24aa4d) to create this from scratch or see how it was built.
+
+It is designed to implement a backend API that can query external APIs with axios. 
+
+## TO GET STARTED:
+
+
+* Make a copy of `env-sample` as `.env` and fill it with the API key for your desired service (if any). 
+* In the root directory, run `npm i` then `npm start`
+* In the `client` directory, run `npm i` then `npm start`
+* View your app at [localhost:3000](https://localhost:3000)
+
+## TO DEPLOY
+
+* Make an app in heroku (e.g. `insert-your-app-name-here`)
+* Add any required environment variables from `.env` to heroku (settings > config vars)
+* Commit all your code to git
+* Install (`sudo npm i -g heroku`) and log in to the heroku cli and push your code to your heroku app
+
+```
+heroku login
+heroku git:remote -a insert-your-app-name-here
+git push heroku main
+```
+
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const express = require("express");
+let dotenv = require("dotenv").config();
+const axios = require("axios");
 
 const PORT = process.env.PORT || 3001;
+const API_KEY = process.env.API_KEY; //CHANGE THIS IN THE .ENV
+const API_ENDPOINT = "https://myapi.com/api/v1/data/"; //FILL THIS IN
 
 const app = express();
 
@@ -13,7 +17,37 @@ app.get("/api", (req, res) => {
   res.json({ message: "Connected to Server" });
 });
 
+app.get("/getExternalEndpoint/:data", (req, res) => {
+  const data = req.params.data || "";
+
+  axios
+    .get(`https://${API_ENDPOINT}]/${data}`, {
+      headers: {
+        "API-KEY": API_KEY, //change this to whatever your API requires in the headers
+        Accept: "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      //return any relevant data here
+      res.json({
+        data: response,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({
+        error: `there was an error with your request`,
+        details: err,
+      });
+    });
+});
+
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
